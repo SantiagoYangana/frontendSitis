50b4c3aa6a6875d1a93d41bb3e05152a76321912
Santiago Montoya <santiagomontoya317@gmail.com>
2023-05-14 14:47:04 -0500

    Listar v4.1 fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateUserComponent } from './components/create-user/create-user.component';
-import { ListUserComponent } from './components/list-user/list-user.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 
@@ -9,7 +8,6 @@ import { HomeComponent } from './components/home/home.component';
 const routes: Routes = [
   {path:'',redirectTo:'login',pathMatch:'full'},
   {path:'createUser',component:CreateUserComponent},
-  {path:'listUser',component:ListUserComponent},
   {path:'login',component:LoginComponent},
   {path:'home',component:HomeComponent}
 ];
